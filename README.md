e90874ff6257551ba40a7c3acedb1c80698b7d37
cryptoAlgorithm <cryptoalgro@gmail.com>
2024-03-18 17:43:55 +0800

    feat: add typealiases for less verbose usage
    feat(README): add basic README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Discord Protobufs for Swift
+
+> Discord Protobufs, compiled for Swift as a package
+
+Compiled Protobufs from [discord-protos](https://github.com/dolfies/discord-protos/)
+as a Swift package.
+
+Mainly designed for consumption by Swiftcord & friends.
--- Sources/SwiftDiscordProtos/SwiftDiscordProtos.swift
+++ Sources/SwiftDiscordProtos/SwiftDiscordProtos.swift
@@ -1,1 +1,8 @@
+/// General Discord user settings
+public typealias PreloadedUserSettings = DiscordProtos_DiscordUsers_V1_PreloadedUserSettings_PreloadedUserSettings
 
+/// Frecency and favorites storage
+public typealias FrecencyUserSettings = DiscordProtos_DiscordUsers_V1_FrecencyUserSettings_FrecencyUserSettings
+
+/// Settings for specific guilds
+public typealias GuildSettings = DiscordProtos_DiscordUsers_V1_GuildSettings_GuildSettings
